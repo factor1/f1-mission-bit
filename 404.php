12798b4e68cefd8827675eb8fd24b6b7657b7b26
Melanie Rhodes <melanierhodes@iMac-2019.local>
2021-12-16 11:18:27 -0700

    Created basic 404

--- 404.php
+++ 404.php
@@ -1,15 +1,16 @@
 <?php
-  /**
-   * The 404 Not Found template.
-   *
-   * Used when WordPress encounters an unknown URL.
-   */
-  get_header();
-?>
+/**
+ * The 404 Not Found template.
+ *
+ * Used when WordPress encounters an unknown URL.
+ * 
+ * @package F1 Mission Bit 
+ * @author Factor1 Studios
+ * @since 0.0.1
+ */
+  
+get_header();
 
-  <section class="error-404">
+get_template_part('parts/global/centered-text-block');
 
-  </section>
-
-<?php
-  get_footer();
+get_footer(); ?>
